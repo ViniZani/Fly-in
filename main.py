eea9c10885a62dad4f8acf8664752a68fdd9139a
Vinicius Zani <vinicius.zani04@gmail.com>
2026-05-04 12:20:20 -0300

    feature Class Hub and Connection OK

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from parser_config import Parser
+from parser_config import Parser, Hub, Connections
 import sys
 
 
@@ -8,10 +8,24 @@ if __name__ == "__main__":
         parser = Parser(sys.argv[1])
         (nb_drones, start_hub, list_hubs,
          end_hub, list_conects) = parser.get_data()
-        print("nb_hubs:", nb_drones)
-        # print("start_hub:", start_hub)
-        # print("lista de hubs:", list_hubs)
-        # print("end_hub:", end_hub)
-        print("conexions:", list_conects)
+        start_hub = Hub(**start_hub)
+        end_hub = Hub(**end_hub)
+        list_class_hubs = {}
+        list_class_hubs[start_hub.name] = start_hub
+        list_class_hubs[end_hub.name] = end_hub
+        for h in list_hubs:
+            class_hub = Hub(**h)
+            list_class_hubs[class_hub.name] = class_hub
+        for c in list_conects:
+            hub_a = list_class_hubs[c['a']]
+            hub_b = list_class_hubs[c['b']]
+            conec_for_b = Connections(
+             target=hub_b,
+             max_link_capacity=c['max_link_capacity'])
+            hub_a.connections.append(conec_for_b)
+            conec_for_a = Connections(
+                target=hub_a,
+                max_link_capacity=c['max_link_capacity'])
+            hub_b.connections.append(conec_for_a)
     # except Exception:
-        print("Must have contain one txt")
+        # print("Must have contain one txt")
--- parser_config.py
+++ parser_config.py
@@ -1,18 +1,34 @@
 class Hub:
-    "Nó do grafo, representa cada zona"
-    pass
+    "Nó do grafo, representa cada zona onde o drone pode ficar"
+    def __init__(self, name: str, x: int, y: int, zone: str = 'normal',
+                 color: str = None, max_drones: int = 1):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.zone = zone
+        self.color = color
+        self.max_drones = max_drones
+        self.connections: list['Connections'] = []
 
 
 class Connections:
-    "representa cada aresta do grafo"
-    pass
-
-
-class Drone:
-    pass
+    "representa cada aresta do grafo, conectando 2 ou mais zonas"
+    def __init__(self, target: 'Hub', max_link_capacity: int = 1):
+        self.target = target
+        self.max_link_capacity = 1
+        self.current_occupancy = 0
 
 
 class Graph:
+    "Monta o modelo do grafo"
+    def __init__(self, graphs_hubs, vertex, start_hub, end_hub):
+        self.graphs_hubs = graphs_hubs
+        self.start = start_hub
+        self.end = end_hub
+        self.vertex = vertex
+
+
+class Drone:
     pass
 
 
@@ -79,7 +95,7 @@ class Parser:
         end_hub = None
         curr_hub = None
         list_hubs = []
-        connection_model = {"a": None, "b": None, "max_link_capacity": None}
+        connection_model = {"a": None, "b": None, "max_link_capacity": 1}
         list_conects = []
         try:
             with open(self.archive, "r", encoding='utf-8') as archive:
@@ -142,7 +158,6 @@ class Parser:
                         else:
                             main_part = line
                         main_part = main_part.split()
-                        print(main_part)
                         if len(main_part) > 4:
                             raise ValueError("Name of hubs cant have any space"
                                              " Verify if your data is:"
